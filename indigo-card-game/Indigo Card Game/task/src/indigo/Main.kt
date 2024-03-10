5943bea953508fb27688544e3bd2e6c851c83dbc
zadykian <zadykian@outlook.com>
2024-03-10 18:17:04 +0400

    Indigo Card Game: Stage 1/5

--- indigo-card-game/Indigo Card Game/task/src/indigo/Main.kt
+++ indigo-card-game/Indigo Card Game/task/src/indigo/Main.kt
@@ -1,5 +1,21 @@
 package indigo
 
+data class Card(val suit: Char, val rank: String) {
+    override fun toString(): String = "$rank$suit"
+}
+
+object Deck {
+    val suits = listOf('♦', '♥', '♠', '♣')
+    val ranks = listOf("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
+    fun shuffled() : List<Card>
+        = suits
+            .flatMap { suit -> ranks.map { rank -> Card(suit, rank) } }
+            .shuffled()
+}
+
 fun main() {
-    println("Hello World!")
+    fun <T> Iterable<T>.joinWithSpaces() = joinToString(separator = " ")
+    println(Deck.ranks.joinWithSpaces())
+    println(Deck.suits.joinWithSpaces())
+    println(Deck.shuffled().joinWithSpaces())
 }
