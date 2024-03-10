2d79972f68f139c7a2be28137c508a3d15acf3fe
zadykian <zadykian@outlook.com>
2024-03-10 17:26:12 +0400

    updated .gitignore; untracked redundant files

--- unit-converter/Topics/Lambda with receiver/Test if a number is divisible/src/Main.kt
+++
@@ -1,7 +0,0 @@
-val isDivisible: Int.(Int) -> Boolean = { that -> this % that == 0 }
-
-/* Do not change code below */
-fun main() {
-    val (a, b) = readln().split(' ').map { it.toInt() }
-    println(a.isDivisible(b))
-}
--- unit-converter/Topics/Loops  repeat statement/Print a string exactly ten times/src/Main.kt
+++
@@ -1,4 +0,0 @@
-fun main() {
-    val input = readln()
-    repeat(10) { println(input) }
-}
