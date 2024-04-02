3f02aaffae52930d6abe336d000610baeaaca1f3
zadykian <zadykian@outlook.com>
2024-04-02 19:56:37 +0400

    Indigo Card Game: Stage 5/5 - ComputerPlayerTest

---
+++ indigo-card-game/Indigo Card Game/task/test/ComputerPlayerTest.kt
@@ -0,0 +1,34 @@
+import indigo.Card
+import indigo.Computer
+import indigo.Rank
+import indigo.Suit
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+
+data class ChooseCardTestCase(
+    val topCardOnTable: Card?,
+    val cardsInHand: List<Card>,
+    val expectedChosenCard: Card?
+)
+
+class ComputerPlayerTest {
+    @ParameterizedTest
+    @MethodSource("provideTestCases")
+    fun chooseCard(testcase: ChooseCardTestCase) {
+        val computer = Computer()
+        val chosenCard = computer.chooseCard(testcase.topCardOnTable, testcase.cardsInHand)
+        Assertions.assertEquals(testcase.expectedChosenCard, chosenCard)
+    }
+
+    companion object {
+        @JvmStatic
+        fun provideTestCases() = listOf(
+            ChooseCardTestCase(
+                topCardOnTable = null,
+                cardsInHand = listOf(Card(Suit.Clubs, Rank.Ace)),
+                expectedChosenCard = Card(Suit.Clubs, Rank.Ace)
+            )
+        )
+    }
+}
