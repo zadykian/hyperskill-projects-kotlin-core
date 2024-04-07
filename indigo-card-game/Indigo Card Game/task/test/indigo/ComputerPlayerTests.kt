385dca0fd45ff593d3a786f6864fad51e0deeeca
zadykian <zadykian@outlook.com>
2024-04-07 21:33:09 +0400

    Indigo Card Game: Stage 5/5 - ComputerPlayerTests; CardsGenerator.kt (kotlinpoet)

--- indigo-card-game/Indigo Card Game/task/test/indigo/ComputerPlayerTests.kt
+++ indigo-card-game/Indigo Card Game/task/test/indigo/ComputerPlayerTests.kt
@@ -1,33 +1,68 @@
 package indigo
 
-import indigo.generated.Cards
-import org.junit.jupiter.api.Assertions
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.isIn
+import indigo.generated.cards.*
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 
+sealed class Expects private constructor() {
+    class Specific(val card: Card) : Expects()
+    class AnyOf(vararg cards: Card) : Expects() {
+        val cards = cards.toSet()
+
+        init {
+            require(cards.isNotEmpty())
+        }
+    }
+}
+
 data class ChooseCardTestCase(
+    val name: String,
     val topCardOnTable: Card?,
-    val cardsInHand: List<Card>,
-    val expectedChosenCard: Card?
-)
+    val cardsInHand: Set<Card>,
+    val expected: Expects
+) {
+    override fun toString() = name
+}
 
 class ComputerPlayerTests {
     @ParameterizedTest
-    @MethodSource("provideTestCases")
+    @MethodSource("testCases")
     fun chooseCard(testcase: ChooseCardTestCase) {
         val computer = Computer()
-        val chosenCard = computer.chooseCard(testcase.topCardOnTable, testcase.cardsInHand)
-        Assertions.assertEquals(testcase.expectedChosenCard, chosenCard)
+        val chosenCard = computer.chooseCard(testcase.topCardOnTable, testcase.cardsInHand.toList())
+
+        assertThat(chosenCard).let {
+            when (testcase.expected) {
+                is Expects.Specific -> it.isEqualTo(testcase.expected.card)
+                is Expects.AnyOf -> it.isIn(testcase.expected.cards)
+            }
+        }
     }
 
     companion object {
         @JvmStatic
-        fun provideTestCases() = listOf(
+        fun testCases() = listOf(
             ChooseCardTestCase(
+                name = "One card in hand; Empty table",
                 topCardOnTable = null,
-                cardsInHand = listOf(Cards.aceOfClubs),
-                expectedChosenCard = Cards.aceOfClubs
-            )
+                cardsInHand = setOf(aceOfClubs),
+                expected = Expects.Specific(aceOfClubs)
+            ),
+            ChooseCardTestCase(
+                name = "One card in hand; Non-empty table",
+                topCardOnTable = Deck.allCards.random(),
+                cardsInHand = setOf(fiveOfHearts),
+                expected = Expects.Specific(fiveOfHearts)
+            ),
+            ChooseCardTestCase(
+                name = "One candidate card",
+                topCardOnTable = fiveOfDiamonds,
+                cardsInHand = setOf(twoOfClubs, aceOfHearts, fiveOfDiamonds, kingOfSpades),
+                expected = Expects.Specific(fiveOfDiamonds)
+            ),
         )
     }
 }
