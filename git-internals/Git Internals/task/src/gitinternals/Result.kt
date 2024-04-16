96c20adc5da04aa478584bd0df52672a12138bc3
zadykian <zadykian@outlook.com>
2024-04-17 01:00:47 +0400

    Git Internals 2/5 (WIP)

--- git-internals/Git Internals/task/src/gitinternals/Result.kt
+++ git-internals/Git Internals/task/src/gitinternals/Result.kt
@@ -6,23 +6,17 @@ class Success<T>(val value: T) : Result<T>
 
 class Failure<T>(val errorMessage: String) : Result<T>
 
-interface IO {
-    fun read(): String
-    fun write(value: String)
-
-    fun <T> run(scope: IO.() -> Result<T>): Result<T> = scope(this@IO)
-
-    fun <T, R> Result<T>.bind(transform: (T) -> Result<R>) = when(this) {
-        is Success -> try {
-            transform(value)
-        } catch (e: Throwable) {
-            Failure(e.localizedMessage)
-        }
-        is Failure -> Failure(this.errorMessage)
+fun <T, R> Result<T>.bind(transform: (T) -> Result<R>) = when(this) {
+    is Success -> try {
+        transform(value)
+    } catch (e: Throwable) {
+        Failure(e.localizedMessage)
     }
-
-    fun <T> Result<T>.onFailure(handle: IO.(String) -> Unit) = when(this) {
-        is Success -> Unit
-        is Failure -> handle(this@IO, this@onFailure.errorMessage)
-    }
+    is Failure -> Failure(this.errorMessage)
 }
+
+fun <T> Result<T>.onFailure(handle: (String) -> Unit) = when(this) {
+    is Success -> Unit
+    is Failure -> handle(this.errorMessage)
+}
+
