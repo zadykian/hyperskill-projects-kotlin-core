3897bc527f0e905c1b2ef6e962d2cdcd585984b6
zadykian <zadykian@outlook.com>
2024-06-12 17:21:15 +0400

    contacts 4/4: WIP 1. removed Error.DynamicInvocationFailed type

--- contacts/Contacts (Kotlin)/task/src/Error.kt
+++ contacts/Contacts (Kotlin)/task/src/Error.kt
@@ -4,11 +4,9 @@ import arrow.core.raise.Raise
 
 typealias RaiseAnyError = Raise<Error>
 typealias RaiseInvalidInput = Raise<Error.InvalidInput>
-typealias RaiseDynamicInvocationFailed = Raise<Error.DynamicInvocationFailed>
 
 sealed class Error(val displayText: CharSequence) {
     class InvalidInput(displayText: CharSequence) : Error(displayText)
-    class DynamicInvocationFailed(displayText: CharSequence) : Error(displayText)
     class Aggregate(errors: List<Error>) : Error(errors.joinToString("\n") { it.displayText })
 
     companion object {
